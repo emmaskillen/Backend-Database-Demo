8ba193b136cfe9a181c774df9487dbaad7e09665
emmaskillen <emma.skillen28@gmail.com>
2023-08-18 12:21:20 -0700

    fixed db connection

--- database/db-connector.js
+++ database/db-connector.js
@@ -6,10 +6,10 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : '***',
-    password        : '***',
-    database        : '***'
+    host            : 'localhost:3306',
+    user            : 'skillene',
+    password        : 'Mimi1918!',
+    database        : 'skillene'
 })
 
 // Export it for use in our applicaiton
